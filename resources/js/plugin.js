0f8ff76512b2a50dfe235018d95f81cd2620861c
Gurinder Chauhan <gurinderchauhan@gmail.com>
2023-10-10 10:43:16 -0400

    WIP

--- resources/js/plugin.js
+++ resources/js/plugin.js
@@ -1,97 +1,88 @@
-function randomName() {
-    return (
-        [1e7] +
-        -1e3 +
-        -4e3 +
-        -8e3 +
-        -1e11
-    ).replace(/[018]/g, (c) =>
-        (
-            c ^
-            (crypto.getRandomValues(new Uint8Array(1))[0] &
-                (15 >> (c / 4)))
-        ).toString(16),
-    );
-}
+// function randomName() {
+//     return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>(c ^(crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4))) ).toString(16),);
+// }
 
-export default function apsonexSimpleFileField(state, args) {
-    return {
-        id: null,
-        state: null,
-        statePath: null,
-        wire: null,
-        disabled: false,
-        progress: 0,
-        processing: false,
-        img: { src: null, key: null, blob: null },
-        fileKeyIndex: {},
-        init() {
-            this.id = args.id;
-            this.statePath = args.statePath;
-            this.disabled = args.disabled;
-            this.wire = args.$wire;
-            this.state = state;
-            this.getFiles();
-        },
-        async getFiles() {
-            this.processing = true;
-            // https://github.com/filamentphp/filament/blob/3.x/packages/forms/resources/views/components/file-upload.blade.php
-            // https://github.com/filamentphp/filament/blob/3.x/packages/forms/resources/js/components/file-upload.js#L118
-            let uploadedFiles = await args.getFormUploadedFiles();
-            this.fileKeyIndex = uploadedFiles ?? {}
-            this.img.key = Object.keys(this.fileKeyIndex)[0];
-            this.img.src = this.img.key ? this.fileKeyIndex[this.img.key].url : null;
-            this.processing = false;
-        },
-        async deleteFile() {
-            this.img.blob = null;
+// export default function apsonexSimpleFileField(state, args) {
+//     return {
+//         id: null,
+//         state: null,
+//         statePath: null,
+//         wire: null,
+//         disabled: false,
+//         progress: 0,
+//         processing: false,
+//         img: { src: null, key: null, blob: null },
+//         fileKeyIndex: {},
+//         init() {
+//             this.id = args.id;
+//             this.statePath = args.statePath;
+//             this.disabled = args.disabled;
+//             this.wire = args.$wire;
+//             this.state = state;
+//             this.getFiles();
+//             console.log('one');
+//         },
+//         async getFiles() {
+//             this.processing = true;
+//             // https://github.com/filamentphp/filament/blob/3.x/packages/forms/resources/views/components/file-upload.blade.php
+//             // https://github.com/filamentphp/filament/blob/3.x/packages/forms/resources/js/components/file-upload.js#L118
+//             let uploadedFiles = await args.getFormUploadedFiles();
+//             this.fileKeyIndex = uploadedFiles ?? {}
+//             this.img.key = Object.keys(this.fileKeyIndex)[0];
+//             this.img.src = this.img.key ? this.fileKeyIndex[this.img.key].url : null;
+//             this.processing = false;
+//         },
+//         async deleteFile() {
+//             this.img.blob = null;
 
-            if (!this.img.key) {
-                return;
-            }
+//             if (!this.img.key) {
+//                 return;
+//             }
 
-            await args.deleteUploadedFileUsing(this.img.key);
-            this.img.src = null;
-            this.img.key = null;
-        },
-        uploadFile($event) {
-            this.processing = true;
-            this.progress = 0;
+//             await args.deleteUploadedFileUsing(this.img.key);
+//             this.img.src = null;
+//             this.img.key = null;
+//         },
+//         uploadFile($event) {
+//             this.processing = true;
+//             this.progress = 0;
 
-            this.readFile(
-                $event.target.files[0],
-                (async (file, blob) => {
-                    let fileKey = randomName();
-                    this.processing = true;
+//             this.readFile(
+//                 $event.target.files[0],
+//                 (async (file, blob) => {
+//                     let fileKey = randomName();
+//                     this.processing = true;
 
-                    this.img.blob = blob;
+//                     this.img.blob = blob;
 
-                    await args.uploadUsing(
-                        fileKey,
-                        file,
-                        (fileKey) => {
-                            this.processing = false;
-                            this.img.key = fileKey;
-                        },
-                        (err) => {
-                            console.log('error: ' + err);
-                        },
-                        ($progressEvent) => {
-                            console.log($progressEvent);
-                        }
-                    )
-                }).bind(this)
-            )
-        },
-        uploadSuccess(blob, fileName) {
-            this.img.dataUri = true;
-            this.img.src = blob;
-            this.processing = false;
-        },
-        readFile(file, onSuccess) {
-            var reader = new FileReader();
-            reader.onload = () => onSuccess(file, reader.result);
-            reader.readAsDataURL(file);
-        }
-    };
-}
+//                     await args.uploadUsing(
+//                         fileKey,
+//                         file,
+//                         (fileKey) => {
+//                             this.processing = false;
+//                             this.img.key = fileKey;
+//                         },
+//                         (err) => {
+//                             console.log('error: ' + err);
+//                         },
+//                         ($progressEvent) => {
+//                             console.log($progressEvent);
+//                         }
+//                     )
+//                 }).bind(this)
+//             )
+//         },
+//         uploadSuccess(blob, fileName) {
+//             this.img.dataUri = true;
+//             this.img.src = blob;
+//             this.processing = false;
+//         },
+//         readFile(file, onSuccess) {
+//             var reader = new FileReader();
+//             reader.onload = () => onSuccess(file, reader.result);
+//             reader.readAsDataURL(file);
+//         }
+//     };
+// }
+
+// console.log('loaded');
--- resources/views/components/file.blade.php
+++ resources/views/components/file.blade.php
@@ -15,35 +15,99 @@
     <div
         x-ignore
         ax-load
-        ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('filament-simple-file-js-plugin', 'apsonex/filament-simple-file') }}{{ app()->environment('local') ? '&cache=' . now()->getTimestamp() : '' }}"
         x-load-css="[
             '{{ \Filament\Support\Facades\FilamentAsset::getStyleHref('filament-simple-file-css-plugin', 'apsonex/filament-simple-file') }}{{ app()->environment('local') ? '&cache=' . now()->getTimestamp() : '' }}'
         ]"
-        {{-- ax-load-src="{{ filament_asset_route('resources/dist/plugin.js', FilamentSimpleFileServiceProvider::class) }}" --}}
-        x-data="apsonexSimpleFileField(
-            $wire.$entangle('{{ $statePath }}'),
-            {
-                $wire: $wire,
-                disabled: {{ $isDisabled ? 'true' : 'false' }},
-                id: '{{ $id }}',
-                statePath: '{{ $statePath }}',
-                getFormUploadedFiles: async () => await $wire.getFormUploadedFiles(@js($statePath)),
-                uploadUsing: (fileKey, file, success, error, progress) => {
-                    $wire.upload(
-                        `{{ $statePath }}.${fileKey}`,
-                        file,
-                        () => success(fileKey),
-                        error,
-                        (progressEvent) => progress(true, progressEvent.detail.progress, 100),
-                    )
-                },
-                deleteUploadedFileUsing: async (fileKey) => await $wire.deleteUploadedFile(@js($statePath), fileKey),
-                removeUploadedFileUsing: async (fileKey) => await $wire.removeFormUploadedFile(@js($statePath), fileKey),
-                state: $wire.$entangle('{{ $statePath }}'),
-                maxSize: {{ $maxSize }},
-                minSize: {{ $minSize }},
+        x-data="{
+            randomName: () => ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>(c ^(crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4))) ).toString(16),),
+            disabled: {{ $isDisabled ? 'true' : 'false' }},
+            id: '{{ $id }}',
+            statePath: '{{ $statePath }}',
+            getFormUploadedFiles: async () => await $wire.getFormUploadedFiles(@js($statePath)),
+            uploadUsing: (fileKey, file, success, error, progress) => {
+                $wire.upload(
+                    `{{ $statePath }}.${fileKey}`,
+                    file,
+                    () => success(fileKey),
+                    error,
+                    (progressEvent) => progress(true, progressEvent.detail.progress, 100),
+                )
+            },
+            deleteUploadedFileUsing: async (fileKey) => await $wire.deleteUploadedFile(@js($statePath), fileKey),
+            removeUploadedFileUsing: async (fileKey) => await $wire.removeFormUploadedFile(@js($statePath), fileKey),
+            state: $wire.$entangle('{{ $statePath }}'),
+            maxSize: {{ $maxSize }},
+            minSize: {{ $minSize }},
+            state: null,
+            wire: null,
+            progress: 0,
+            processing: false,
+            img: { src: null, key: null, blob: null },
+            fileKeyIndex: {},
+            init() {
+                this.getFiles();
+            },
+            async getFiles() {
+                this.processing = true;
+                // https://github.com/filamentphp/filament/blob/3.x/packages/forms/resources/views/components/file-upload.blade.php
+                // https://github.com/filamentphp/filament/blob/3.x/packages/forms/resources/js/components/file-upload.js#L118
+                let uploadedFiles = await this.getFormUploadedFiles();
+                this.fileKeyIndex = uploadedFiles ?? {}
+                this.img.key = Object.keys(this.fileKeyIndex)[0];
+                this.img.src = this.img.key ? this.fileKeyIndex[this.img.key].url : null;
+                this.processing = false;
+            },
+            async deleteFile() {
+                this.img.blob = null;
+
+                if (!this.img.key) {
+                    return;
+                }
+
+                await this.deleteUploadedFileUsing(this.img.key);
+                this.img.src = null;
+                this.img.key = null;
+            },
+            uploadFile($event) {
+                this.processing = true;
+                this.progress = 0;
+
+                this.readFile(
+                    $event.target.files[0],
+                    (async (file, blob) => {
+                        let fileKey = this.randomName();
+                        this.processing = true;
+
+                        this.img.blob = blob;
+
+                        await this.uploadUsing(
+                            fileKey,
+                            file,
+                            (fileKey) => {
+                                this.processing = false;
+                                this.img.key = fileKey;
+                            },
+                            (err) => {
+                                console.log('error: ' + err);
+                            },
+                            ($progressEvent) => {
+                                console.log($progressEvent);
+                            }
+                        )
+                    }).bind(this)
+                )
+            },
+            uploadSuccess(blob, fileName) {
+                this.img.dataUri = true;
+                this.img.src = blob;
+                this.processing = false;
+            },
+            readFile(file, onSuccess) {
+                var reader = new FileReader();
+                reader.onload = () => onSuccess(file, reader.result);
+                reader.readAsDataURL(file);
             }
-        )"
+        }"
     >
         <div
             class="relative flex flex-col flex-wrap"
